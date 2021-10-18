1e137d33adc063abbf99cf460387d63bbbfb95f2
UIsystem-code <76796784+UIsystem-code@users.noreply.github.com>
2021-10-18 18:57:07 +0700

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,15 @@
+# loop
+let input = 'turpentine and turtles';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < 4; i += 1) {
+console.log('i is '+ i);
+};
+/* 
+output =
+i is 0
+i is 1
+i is 2
+i is 3
+*/
